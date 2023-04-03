ec793f7666121dfb96da5deacf5ec31b77f9063a
Rafał Krupiński <rafiwts@gmail.com>
2023-04-03 22:40:15 +0200

    Adding a search form

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -13,3 +13,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment # form is created automatically by Django after analyzing a given model
         fields = ['name', 'email', 'body'] # three columns are taken into account 
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField()
